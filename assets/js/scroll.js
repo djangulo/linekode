16ed9f59b3446be9138d5cf0e4a0fdd8daa27fb5
Denis Angulo <denis.angulo@linekode.com>
2018-01-08 15:07:39 -0500

    added scroll.js template

---
+++ assets/js/scroll.js
@@ -0,0 +1,23 @@
+const value = document.getElementById('value');
+const why = document.getElementById('why');
+const proof = document.getElementById('proof');
+const work = document.getElementById('work');
+
+const navLinks = document.querySelectorAll('nav ul li a');
+
+
+function listenClick(elem, destElem) {
+    elem.addEventListener('click', function (event) {
+            destElem.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+            event.preventDefault();
+        }
+    );
+};
+
+listenClick(navLinks[0], value);
+listenClick(navLinks[1], why);
+listenClick(navLinks[2], proof);
+listenClick(navLinks[3], work);
