8c8e39a41435aabaf05f743651fcb6a5c58f1b89
Denis Angulo <denis.angulo@linekode.com>
2018-01-09 13:25:01 -0500

    linekode/views.py: changed from CBV to function based view, in order to decorate it with csrf magic

--- linekode/views.py
+++ linekode/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
+# class HomeView(TemplateView):
+#     template_name = 'home.html'
+
+
+@ensure_csrf_cookie
+def home(request):
+    return render(request, 'home.html')
