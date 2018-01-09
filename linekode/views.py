8a71adc4cce40e086bdcc6d947976ce24a262de9
Denis Angulo <denis.angulo@linekode.com>
2018-01-09 15:17:48 -0500

    contact form is now rendered in template, added context variables for translations

--- linekode/views.py
+++ linekode/views.py
@@ -1,12 +1,17 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
 from django.views.decorators.csrf import ensure_csrf_cookie
-
-
-# class HomeView(TemplateView):
-#     template_name = 'home.html'
-
 
 @ensure_csrf_cookie
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'first_name': _('First name'),
+        'last_name': _('Last name'),
+        'email': 'Email',
+        'telephone': _('Telephone'),
+        'message': _('Message'),
+        'submit': _('Submit'),
+        'clear': _('Clear'),
+        'your_message_here': _('Your message here'),
+    })
