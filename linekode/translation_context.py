622a49541eedfce925ca2b55469cc42f5d831626
Denis Angulo <denis.angulo@linekode.com>
2018-01-10 12:54:51 -0500

    added translation_context.py file, so makemessages can detect my damn targets without adding context overhead to view

---
+++ linekode/translation_context.py
@@ -0,0 +1,18 @@
+from django.utils.translation import gettext_lazy as _
+
+context = {
+    'value': _('Value'),
+    'why': _('Why?'),
+    'contact': _('Contact'),
+    'work': _('Work'),
+    'inspirational_message': _('There are no limits when you love your project. Expanding your project is easier than ever.'),
+    'why_us': _('Why us?'),
+    'first_name': _('First name(s)'),
+    'last_name': _('Last name(s)'),
+    'email': _('Email'),
+    'telephone': _('Phone'),
+    'message': _('Message'),
+    'your_message_here': _('Your message here'),
+    'submit': _('Submit'),
+    'clear': _('Clear'),
+}
