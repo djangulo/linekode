b0662353ca8175a33d5c61b6f60e3a882107acf9
Denis Angulo <denis.angulo@linekode.com>
2018-01-22 14:07:26 -0500

    removed unused code from home.js, added new layout.html file

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,10 +1,1 @@
 import './../sass/home.sass'
-
-function component() {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    return element
-}
-
